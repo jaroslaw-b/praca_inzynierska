bf3586c915d2b37efbd9f1185fa5a370f25818de
jarcyk <jarcyk1994@gmail.com>
2017-09-11 21:51:49 +0200

    Dodane separowanie obszaru jezdni na podstawie linii skrajnych

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import visionAlgorithm
 wizyjny = visionAlgorithm.VisionAlgorithm()
 wizyjny.load_image()
 wizyjny.analyse_image()
-wizyjny.show_image(wizyjny.image_resized)
-wizyjny.show_image(wizyjny.image_temp)
+wizyjny.show_image(wizyjny.image_to_display)
--- visionAlgorithm.py
+++ visionAlgorithm.py
@@ -1,22 +1,46 @@
 import cv2
-import numpy as np
+
 
 class VisionAlgorithm:
+
+    IMG_WIDTH = 640
+    IMG_HEIGHT = 480
+    
     def __init__(self):
-        self.image_temp = []
-        self.image_temp1 = []
-    def load_image(self):
-        self.image_raw = cv2.imread("test_images/test_image_5.jpg")
-        self.image_resized = cv2.resize(self.image_raw, (640, 480))
-        for i in range(1,640):
-            for j in range(1, 320):
-                self.image_resized[j][i] = 0
-    def show_image(self, image):
-        cv2.imshow("image_raw", image)
-        cv2.waitKey(0)
-    def analyse_image(self):
-        self.image_hsv = cv2.cvtColor(self.image_resized, cv2.COLOR_BGR2HSV_FULL)
-        image2analyse = self.image_hsv[:,:,2]
-        self.image_temp = cv2.Canny(image2analyse, 40, 500)
-        self.image_temp = cv2.copyMakeBorder(self.image_temp, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value = [255, 255, 255])
+        self.image_to_display = []
+        self.image_in_process = []
         
+    def load_image(self):
+        
+        image_raw = cv2.imread("test_images/test_image_5.jpg")
+        image_resized = cv2.resize(image_raw, (self.IMG_WIDTH, self.IMG_HEIGHT))
+        for i in range(1, self.IMG_WIDTH):
+            for j in range(1, self.IMG_HEIGHT-180):
+                image_resized[j][i] = (255, 255, 0)
+        self.image_in_process = image_resized
+
+    @staticmethod
+    def show_image(image):
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+        
+    def analyse_image(self):
+        img_w = self.IMG_WIDTH
+        img_h = self.IMG_HEIGHT
+        image_hsv = cv2.cvtColor(self.image_in_process, cv2.COLOR_BGR2HSV_FULL)
+        image2analyse = image_hsv[:, :, 2]
+        image_temp = cv2.Canny(image2analyse, 40, 500)
+        image_temp = cv2.copyMakeBorder(image_temp, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=255)
+        mask = cv2.resize(image_temp, (img_w+2, img_h+2))
+        image_temp = cv2.resize(image_temp, (img_w, img_h))
+        cv2.floodFill(image_temp, mask, (320, int(img_w/2)), 255)
+        morph_element = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        image_temp = cv2.erode(image_temp, morph_element, iterations=2)
+        image_temp = cv2.dilate(image_temp, morph_element, iterations=2)
+        var, image_temp = cv2.threshold(image_temp, 10, 255, cv2.THRESH_BINARY)
+        image_road = self.image_in_process
+        for i in range(0, img_w):
+            for j in range(0, img_h):
+                if image_temp[j][i] == 0:
+                    image_road[j][i] = (255, 255, 0)
+        self.image_to_display = image_road
